4f134d7cb2413d9cf9e4d3543bd4e01b9614949a
krishnatest406 <krishnatest406@gmail.com>
2023-02-28 21:01:12 +0530

    pro 1

---
+++ src/test/java/projectjenkins/Class1.java
@@ -0,0 +1,31 @@
+package projectjenkins;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Class1 {
+
+	@Test
+	public void getpageTitleOfFb() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		RemoteWebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		if(driver.getTitle().contains("facebook")) {
+			System.out.println("Browser opened..!");
+		}
+		else {
+			System.out.println("Browser not opened..!");
+		}
+			
+		Thread.sleep(2000);
+		driver.close();
+	}
+	
+
+		
+	
+}
